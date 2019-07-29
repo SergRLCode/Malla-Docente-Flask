7b17f875cc9fcce0cf97f8e03800cc56257a626c
Sergio Romero <ing.isc.srl@gmail.com>
2019-07-29 11:29:21 -0500

    changes

--- routes.py
+++ routes.py
@@ -167,18 +167,20 @@ def courses_by_period(period):
 @jwt_required
 def available_courses():            # Ruta que retorna una lista con los cursos disponibles, siendo el dia de inicio mayor a la fecha del servidor
     if(request.method=='GET'):
+        _rfc = ""
         availableCourses = Course.objects.filter(dateStart__gte=dt.now().date()).values_list('courseName', 'teacherRFC', 'timetable', 'teachersInCourse', 'state')
         if get_jwt_identity()[1] != 0 and get_jwt_identity()[1] != 1:
             myCourses = Course.objects.filter(teacherRFC=get_jwt_identity()[0]).values_list('courseName')
             coursesRequested = RequestCourse.objects.filter(requests__contains=get_jwt_identity()[0]).values_list('course')
             coursesRejected = BlacklistRequest.objects.filter(requests__contains=get_jwt_identity()[0]).values_list('course')
+            _rfc = get_jwt_identity()[0]
         else:
             myCourses = []
             coursesRejected = []
             coursesRequested = []
         arrayToSend = []
         for vals in availableCourses:
-            if get_jwt_identity()[0] not in vals[3] and vals[0] not in coursesRequested and vals[0] not in coursesRejected and vals[0] not in myCourses:
+            if _rfc not in vals[3] and vals[0] not in coursesRequested and vals[0] not in coursesRejected and vals[0] not in myCourses:
                 teacherName = Teacher.objects.filter(rfc=vals[1]).values_list('name', 'fstSurname', 'sndSurname')
                 completeName = "{} {} {}".format(teacherName[0][0], teacherName[0][1], teacherName[0][2])
                 arrayToSend.append({
