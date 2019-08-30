e1ef7f59359a6c1a797067431a90eb623a0c6578
Sergio Romero <ing.isc.srl@gmail.com>
2019-08-30 11:02:29 -0500

    changes

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import redis
 import sys
 
 app = Flask(__name__)
-mongoengine.connection.disconnect()
 app.config['MONGODB_SETTINGS'] = {'db':'Capacitacion', 'alias':'default'}
 app.config['JWT_SECRET_KEY'] = 'uCm3uZm1kGPlB7ATTlsMoA'
 app.config['JWT_BLACKLIST_ENABLED'] = True
@@ -19,14 +18,13 @@ mongoengine.connect(
     host='mongodb+srv://sergioRL:invierno%5F1@databases-k71qn.gcp.mongodb.net/test?retryWrites=true&w=majority'
 )
 
-
 redis = redis.StrictRedis(password='invierno')
-mongoengine.connection.disconnect()
+# mongoengine.connection.disconnect()
 db = MongoEngine(app)
 jwt = JWTManager(app)
 CORS(app)
 
-mongoengine.connection.disconnect()
+# disconnect()
 from routes_endpoints import *
 
 message = """
