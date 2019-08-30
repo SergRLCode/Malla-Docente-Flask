fc6cdd8d2ca1118ae1ed16836effa2bc69937628
Sergio Romero <ing.isc.srl@gmail.com>
2019-08-30 10:46:13 -0500

    changes

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from mongoengine import connect, disconnect
 from flask_jwt_extended import JWTManager
 from flask_mongoengine import MongoEngine
 from flask_cors import CORS
 from flask import Flask
+import mongoengine
 import redis
 import sys
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {'db':'Capacitacion', 'alias':'default'}
+app.config['MONGODB_SETTINGS'] = {'db':'Capacitacion', 'db_alias':'default'}
 app.config['JWT_SECRET_KEY'] = 'uCm3uZm1kGPlB7ATTlsMoA'
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
-connect(
+mongoengine.connect(
     host='mongodb+srv://sergioRL:invierno%5F1@databases-k71qn.gcp.mongodb.net/test?retryWrites=true&w=majority'
 )
 
 redis = redis.StrictRedis(password='invierno')
-disconnect()
+# disconnect()
 db = MongoEngine(app)
 jwt = JWTManager(app)
 CORS(app)
 
-disconnect()
+# disconnect()
 from routes_endpoints import *
 
 message = """
